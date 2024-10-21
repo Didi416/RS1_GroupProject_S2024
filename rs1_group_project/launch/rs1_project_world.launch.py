7aeaf771c0211bd45ca104b15c169ddeb1351c0c
Didi416 <didiprins@optusnet.com.au>
2024-10-21 08:11:23 +0000

    Group Project Launch File

---
+++ rs1_group_project/launch/rs1_project_world.launch.py
@@ -0,0 +1,139 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import SetEnvironmentVariable
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+    pkg_rs1_project = get_package_share_directory('rs1_project')
+    pkg_turtlebot3 = os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'launch')
+
+    world = os.path.join(pkg_rs1_project, 'worlds', 'rs1_project_world.world')
+
+    map_dir = LaunchConfiguration(
+        'map',
+        default=os.path.join(
+            get_package_share_directory('turtlebot3_navigation2'),
+            'map',
+            'map.yaml'))
+
+    param_file_name = 'waffle_pi.yaml'
+    param_dir = LaunchConfiguration(
+        'params_file',
+        default=os.path.join(
+            get_package_share_directory('turtlebot3_navigation2'),
+            'param',
+            param_file_name))
+
+    nav2_launch_file_dir = os.path.join(get_package_share_directory('nav2_bringup'), 'launch')
+
+    rviz_config_dir = os.path.join(
+        get_package_share_directory('nav2_bringup'),
+        'rviz',
+        'nav2_default_view.rviz')
+    
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true'),
+    x_pose = LaunchConfiguration('x_pose', default='-2.0'),
+    y_pose = LaunchConfiguration('y_pose', default='2.0'),
+    
+    gazebo_LD = LaunchDescription([
+        # Gazebo Launch directives
+        DeclareLaunchArgument(
+            'model',
+            default_value='waffle_pi',
+            description="TurtleBot3 model"
+        ),
+        SetEnvironmentVariable(
+            'TURTLEBOT3_MODEL', LaunchConfiguration('model')),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')
+            ),
+            launch_arguments={'world': world}.items()
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')
+            )
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(pkg_turtlebot3, 'robot_state_publisher.launch.py')
+            ),
+            launch_arguments={'use_sim_time': use_sim_time}.items()
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(pkg_turtlebot3, 'spawn_turtlebot3.launch.py')
+            ),
+            launch_arguments={
+                'x_pose': x_pose,
+                'y_pose': y_pose
+            }.items()
+        ),
+
+        # Nav2 Launch directives
+        DeclareLaunchArgument(
+            'map',
+            default_value=map_dir,
+            description='Full path to map file to load'),
+
+        DeclareLaunchArgument(
+            'params_file',
+            default_value=param_dir,
+            description='Full path to param file to load'),
+
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation (Gazebo) clock if true'),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([nav2_launch_file_dir, '/bringup_launch.py']),
+            launch_arguments={
+                'map': map_dir,
+                'use_sim_time': use_sim_time,
+                'params_file': param_dir}.items(),
+        ),
+
+        # Launch RVIZ Node
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', rviz_config_dir],
+            parameters=[{'use_sim_time': use_sim_time}],
+            output='screen'),
+        
+        # Launch navigation executable node
+        Node(
+            package='rs1_group_project',
+            executable='navigation',
+            name='navigation',
+            output='screen',
+            parameters=[{'param_name': 'param_value'}]  # Optional parameters
+        ),
+    ])
+
+    # gazebo_LD = LaunchDescription()
+    # gazebo_LD.add_action(set_turtlebot_model_cmd)
+    # gazebo_LD.add_action(set_turtlebot_model_env_var)
+    # gazebo_LD.add_action(gzserver_cmd)
+    # gazebo_LD.add_action(gzclient_cmd)
+    # gazebo_LD.add_action(robot_state_publisher_cmd)
+    # gazebo_LD.add_action(spawn_turtlebot_cmd)
+    # gazebo_LD.add_action(nav2_cmd)
+
+
+    return gazebo_LD
